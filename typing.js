a9452decdf53f8f807754e2bdd225f4c57f31274
amrit2603 <theamritkumar263@gmail.com>
2025-02-06 23:09:17 +0530

    first commit

---
+++ typing.js
@@ -0,0 +1,7 @@
+console.log("i dont wanna think about it");
+process.stdout.write("hello bahot marunga bc");
+process.stdout.write("namaste");
+console.log("tora mai ");
+console.log("tora mai ");
+console.table({city: "jaipur"})
+console.warn({ city: "jaipur" });
