0518e134019606a17d2e1567a70934459961fb56
amrit2603 <theamritkumar263@gmail.com>
2025-02-09 23:56:22 +0530

    Update testloop.js

--- part-4/testloop.js
+++ part-4/testloop.js
@@ -1,11 +1,11 @@
-//problem 1
-// let sum = 0;
-// let i = 1;
-// while(i<=5){
-//     sum += i;
-//     i++;
-// }
-// console.log(sum);
+problem 1
+let sum = 0;
+let i = 1;
+while(i<=5){
+    sum += i;
+    i++;
+}
+console.log(sum);
 
 
 
