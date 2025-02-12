853b705f14598a922b4eddafd8e19d133f018180
amrit2603 <theamritkumar263@gmail.com>
2025-02-12 20:27:04 +0530

    Delete typing.js

--- typing.js
+++
@@ -1,7 +0,0 @@
-console.log("i dont wanna think about it");
-process.stdout.write("hello bahot marunga bc");
-process.stdout.write("namaste");
-console.log("tora mai ");
-console.log("tora mai ");
-console.table({city: "jaipur"})
-console.warn({ city: "jaipur" });
