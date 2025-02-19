fadda77ee5fe1e6c58aa45d6fbc942b48cb2240c
amrit2603 <theamritkumar263@gmail.com>
2025-02-20 00:05:09 +0530

    Add files via upload

---
+++ part-6/part-3/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let num1 = 29;
+let num2 = 55
+
+if(num1>num2){
+    console.log("num 1 is greater");
+}
+else{
+    console.log("num 2 is greater");
+    
+}
---
+++ part-6/part-6/function4.js
@@ -0,0 +1,24 @@
+// function makeTea(params) {
+//     return `maketea : ${typeOfTea}`;
+// }
+
+// function processOrder(fn) {
+//    return fn(`earl grey`)
+// }
+
+// let order = processOrder(makeTea)
+// console.log(order);
+
+
+// function createTeaMaker(name) {
+//     return function(teaType){
+//         return `making ${teaType}`
+//     }
+// }
+// let ele = createTeaMaker()
+// console.log(ele("green tea"));
+
+
+function whatAmI(i) {
+  return typeof i;
+}
